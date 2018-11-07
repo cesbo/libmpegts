91fea59822380f8f497c337dd7e2036db4a12d2a
Oleg Golovanov <golovanov.ov@gmail.com>
2018-11-08 01:10:03 +0500

    added get_u13 and set_u13 functions

--- src/base.rs
+++ src/base.rs
@@ -11,6 +11,19 @@ pub fn get_u12(ptr: &[u8]) -> u16 {
     get_u16(ptr) & 0x0FFF
 }
 
+/// Gets 13 bits unsigned integer from byte array
+///
+/// # Examples
+///
+/// ```
+/// use mpegts::base::*;
+/// assert_eq!(get_u13(&[0x32, 0x34]), 0x1234);
+/// ```
+#[inline]
+pub fn get_u13(ptr: &[u8]) -> u16 {
+    get_u16(ptr) & 0x1FFF
+}
+
 /// Gets 16 bits unsigned integer from byte array
 ///
 /// # Examples
@@ -50,6 +63,23 @@ pub fn get_u32(ptr: &[u8]) -> u32 {
 #[inline]
 pub fn set_u12(ptr: &mut [u8], value: u16) {
     let value = value & 0x0FFF;
+    ptr[0] = (ptr[0] & 0xF0) | ((value >> 8) as u8);
+    ptr[1] = (value) as u8;
+}
+
+/// Sets 13 bits unsigned integer to byte array. Preserves first 3 bits in the first byte
+///
+/// # Examples
+///
+/// ```
+/// use mpegts::base::*;
+/// let mut x: Vec<u8> = vec![0xA0, 0x00];
+/// set_u13(x.as_mut_slice(), 0x1234);
+/// assert_eq!(x, [0xB2, 0x34]);
+/// ```
+#[inline]
+pub fn set_u13(ptr: &mut [u8], value: u16) {
+    let value = value & 0x1FFF;
     ptr[0] = (ptr[0] & 0xF0) | ((value >> 8) as u8);
     ptr[1] = (value) as u8;
 }
