ef767ec99a9595a304057953da8480c2dac3073c
Oleg Golovanov <golovanov.ov@gmail.com>
2018-11-09 01:53:41 +0500

    done Pmt.parse

--- src/psi/pmt.rs
+++ src/psi/pmt.rs
@@ -1,3 +1,4 @@
+use base;
 use psi::{Psi, Descriptors};
 
 
@@ -43,7 +44,27 @@ impl Pmt {
     }
 
     pub fn parse(&mut self, psi: &Psi) {
+        if ! self.check(psi) {
+            return;
+        }
 
+        self.version = psi.get_version();
+        self.pnr = base::get_u16(&psi.buffer[3 ..]);
+        self.pcr = base::get_u13(&psi.buffer[8 ..]);
+
+        let program_length = base::get_u12(&psi.buffer[10 ..]) as usize;
+        self.descriptors.parse(&psi.buffer[11 .. 11 + program_length]);
+
+        let ptr = &psi.buffer[12 + program_length .. psi.size - 4];
+        let mut skip = 0;
+        while ptr.len() >= skip + 5 {
+            let item_len = 5 + base::get_u12(&ptr[skip + 3 ..]) as usize;
+            if skip + item_len > ptr.len() {
+                break;
+            }
+            self.items.push(PmtItem::parse(&ptr[skip .. skip + item_len]));
+            skip += item_len;
+        }
     }
 
     pub fn assemble(&self, psi: &mut Psi) {
