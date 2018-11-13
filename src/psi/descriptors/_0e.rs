a0736a99d95d69e0a3aa58db99f22870d91640e7
Oleg Golovanov <golovanov.ov@gmail.com>
2018-11-14 03:02:12 +0500

    done Desc0E

---
+++ src/psi/descriptors/_0e.rs
@@ -0,0 +1,41 @@
+use base;
+
+
+/// Maximum bitrate descriptor.
+#[derive(Debug, Default)]
+pub struct Desc0E {
+    /// The value indicates an upper bound of the bitrate,
+    /// including transport overhead, that will be encountered
+    /// in this program element or program.
+    pub bitrate: u32
+}
+
+impl Desc0E {
+    #[inline]
+    pub fn min_size() -> usize {
+        5
+    }
+
+    pub fn check(slice: &[u8]) -> bool {
+        slice.len() == Self::min_size()
+    }
+
+    pub fn parse(slice: &[u8]) -> Self {
+        Self {
+            bitrate: base::get_u22(&slice[2 ..])
+        }
+    }
+
+    pub fn assemble(&self, buffer: &mut Vec<u8>) {
+        buffer.push(0x0E);
+        buffer.push((Self::min_size() - 2) as u8);
+
+        let skip = buffer.len();
+        buffer.push(0xC0);  // reserved bits
+        buffer.resize(skip + 3, 0x00);
+        {
+            let ptr = buffer.as_mut_slice();
+            base::set_u22(&mut ptr[skip ..], self.bitrate);
+        }   
+    }
+}
