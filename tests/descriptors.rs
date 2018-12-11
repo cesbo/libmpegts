e830205917ff704c6d195d3932b7835524cfd6bf
Andrey Dyldin <and@cesbo.com>
2018-12-12 02:54:38 +0500

    tests: Desc4E

--- tests/descriptors.rs
+++ tests/descriptors.rs
@@ -7,10 +7,12 @@ static DATA_09: &[u8] = &[0x09, 0x04, 0x09, 0x63, 0xe5, 0x01];
 static DATA_0A: &[u8] = &[0x0A, 0x04, 0x65, 0x6e, 0x67, 0x01];
 static DATA_0E: &[u8] = &[0x0e, 0x03, 0xc1, 0x2e, 0xbc];
 static DATA_4D: &[u8] = &[
-    0x4d, 0x18, 0x72, 0x75, 0x73, 0x13, 0x01, 0xc1,
-    0xe2, 0xe0, 0xde, 0xd9, 0xda, 0xd0, 0x20, 0xdd,
-    0xd0, 0x20, 0xb0, 0xdb, 0xef, 0xe1, 0xda, 0xd5,
-    0x2e, 0x00];
+    0x4d, 0x18, 0x72, 0x75, 0x73, 0x13, 0x01, 0xc1, 0xe2, 0xe0, 0xde, 0xd9, 0xda, 0xd0, 0x20, 0xdd,
+    0xd0, 0x20, 0xb0, 0xdb, 0xef, 0xe1, 0xda, 0xd5, 0x2e, 0x00];
+static DATA_4E: &[u8] = &[
+    0x4e, 0x20, 0x00, 0x72, 0x75, 0x73, 0x00, 0x1a, 0x01, 0xb7, 0xd8, 0xdc, 0xd0, 0x20,
+    0xd1, 0xeb, 0xe1, 0xe2, 0xe0, 0xde, 0x20, 0xdf, 0xe0, 0xd8, 0xd1, 0xdb, 0xd8, 0xd6, 0xd0, 0xd5,
+    0xe2, 0xe1, 0xef, 0x2e];
 static DATA_52: &[u8] = &[0x52, 0x01, 0x02];
 
 #[test]
@@ -148,6 +150,43 @@ fn test_4d_assemble() {
     descriptors.assemble(&mut assembled);
 
     assert_eq!(assembled.as_slice(), DATA_4D);
+}
+
+#[test]
+fn test_4e_parse() {
+    let mut descriptors = psi::Descriptors::default();
+    descriptors.parse(DATA_4E);
+
+    let desc = match descriptors.iter().next().unwrap() {
+        psi::Descriptor::Desc4E(v) => v,
+        _ => unreachable!()
+    };
+
+    assert_eq!(desc.number, 0);
+    assert_eq!(desc.last_number, 0);
+    assert_eq!(desc.lang, textcode::StringDVB::from_str("rus", textcode::ISO6937));
+    assert_eq!(desc.text, textcode::StringDVB::from_str("Зима быстро приближается.", textcode::ISO8859_5));
+}
+
+#[test]
+fn test_4e_assemble() {
+    let mut descriptors = psi::Descriptors::default();
+    descriptors.push(
+        psi::Descriptor::Desc4E(
+            psi::Desc4E {
+                number: 0,
+                last_number: 0,
+                lang: textcode::StringDVB::from_str("rus", textcode::ISO6937),
+                items: Vec::new(),
+                text: textcode::StringDVB::from_str("Зима быстро приближается.", textcode::ISO8859_5),
+            }
+        )
+    );
+
+    let mut assembled = Vec::new();
+    descriptors.assemble(&mut assembled);
+
+    assert_eq!(assembled.as_slice(), DATA_4E);
 }
 
 #[test]
