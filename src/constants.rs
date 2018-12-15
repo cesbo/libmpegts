30cc85b9be831361197bdb88ebd8d6c44bda668c
Oleg Golovanov <golovanov.ov@gmail.com>
2018-12-16 03:40:20 +0500

    added modulation constants

--- src/constants.rs
+++ src/constants.rs
@@ -2,13 +2,18 @@ pub const SIDE_WEST: u8 = 0;
 pub const SIDE_EAST: u8 = 1;
 
 pub const POLARIZATION_HORIZONTAL: u8 = 0;
-pub const POLARIZATION_VERTICAL: u8 = 1;
-pub const POLARIZATION_LEFT: u8 = 2;
-pub const POLARIZATION_RIGHT: u8 = 3;
+pub const POLARIZATION_VERTICAL: u8   = 1;
+pub const POLARIZATION_LEFT: u8       = 2;
+pub const POLARIZATION_RIGHT: u8      = 3;
 
 pub const ROF_A035: u8 = 0;
 pub const ROF_A025: u8 = 1;
 pub const ROF_A020: u8 = 2;
+
+pub const MODULATION_AUTO: u8  = 0;
+pub const MODULATION_QPSK: u8  = 1;
+pub const MODULATION_8PSK: u8  = 2;
+pub const MODULATION_16QAM: u8 = 3;
 
 pub const NOT_DEFINED: u8 = 0b0000;
 pub const FEC_1_2: u8     = 0b0001;
