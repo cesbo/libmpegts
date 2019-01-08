c6c54a0c98f52d9ac308d0af996d1d48b136f5b7
Andrey Dyldin <and@cesbo.com>
2019-01-08 16:50:22 +0500

    mjd rename from_mjd() <=> to_mjd()

--- src/mjd.rs
+++ src/mjd.rs
@@ -1,18 +1,18 @@
 /// Converts between Unix Timestamp and Modified Julian Date
 pub trait MJD {
-    fn from_mjd(value: u64) -> u16;
-    fn to_mjd(self) -> u64;
+    fn from_mjd(self) -> u64;
+    fn to_mjd(value: u64) -> u16;
 }
 
 impl MJD for u16 {
     #[inline]
-    fn from_mjd(value: u64) -> u16 {
-        (value / 86400 + 40587) as u16
+    fn from_mjd(self) -> u64 {
+        debug_assert!(self >= 40587);
+        (u64::from(self) - 40587) * 86400
     }
 
     #[inline]
-    fn to_mjd(self) -> u64 {
-        debug_assert!(self >= 40587);
-        (u64::from(self) - 40587) * 86400
+    fn to_mjd(value: u64) -> u16 {
+        (value / 86400 + 40587) as u16
     }
 }
--- tests/mjd.rs
+++ tests/mjd.rs
@@ -2,10 +2,10 @@ use mpegts::mjd::MJD;
 
 #[test]
 fn test_from_mjd() {
-    assert_eq!(u16::from_mjd(750470400), 0xc079);
+    assert_eq!(0xc079u16.from_mjd(), 750470400);
 }
 
 #[test]
 fn test_to_mjd() {
-    assert_eq!(0xc079u16.to_mjd(), 750470400);
+    assert_eq!(u16::to_mjd(750470400), 0xc079);
 }
