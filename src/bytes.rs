4b4d69651d9630e0977f84b9154454c7bf1edef9
Andrey Dyldin <and@cesbo.com>
2019-08-24 02:23:44 +0500

    tests: moved bytes tests into module

--- src/bytes.rs
+++ src/bytes.rs
@@ -5,6 +5,7 @@
 // ASC/libmpegts can not be copied and/or distributed without the express
 // permission of Cesbo OU
 
+
 /// Bytes operations with array
 pub trait Bytes {
     fn get_u16(&self) -> u16;
@@ -14,6 +15,7 @@ pub trait Bytes {
     fn get_u32(&self) -> u32;
     fn set_u32(&mut self, value: u32);
 }
+
 
 impl Bytes for [u8] {
     #[inline]
@@ -56,5 +58,59 @@ impl Bytes for [u8] {
         self[1] = (value >> 16) as u8;
         self[2] = (value >> 8) as u8;
         self[3] = (value) as u8;
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use crate::bytes::*;
+
+    #[test]
+    fn test_get_bytes_u16() {
+        let data: &[u8] = &[0x12, 0x34];
+        assert_eq!(data[0 ..].get_u16(), 0x1234);
+    }
+
+    #[test]
+    fn test_set_bytes_u16() {
+        let mut data = Vec::<u8>::new();
+        data.resize(2, 0x00);
+        data[0 ..].set_u16(0x1234);
+        assert_eq!(data[0], 0x12);
+        assert_eq!(data[1], 0x34);
+    }
+
+    #[test]
+    fn test_get_bytes_u24() {
+        let data: &[u8] = &[0x12, 0x34, 0xAB];
+        assert_eq!(data[0 ..].get_u24(), 0x1234AB);
+    }
+
+    #[test]
+    fn test_set_bytes_u24() {
+        let mut data = Vec::<u8>::new();
+        data.resize(3, 0x00);
+        data[0 ..].set_u24(0x1234AB);
+        assert_eq!(data[0], 0x12);
+        assert_eq!(data[1], 0x34);
+        assert_eq!(data[2], 0xAB);
+    }
+
+    #[test]
+    fn test_get_bytes_u32() {
+        let data: &[u8] = &[0x12, 0x34, 0xAB, 0xCD];
+        assert_eq!(data[0 ..].get_u32(), 0x1234ABCD);
+    }
+
+    #[test]
+    fn test_set_bytes_u32() {
+        let mut data = Vec::<u8>::new();
+        data.resize(4, 0x00);
+        data[0 ..].set_u32(0x1234ABCD);
+        assert_eq!(data[0], 0x12);
+        assert_eq!(data[1], 0x34);
+        assert_eq!(data[2], 0xAB);
+        assert_eq!(data[3], 0xCD);
     }
 }
