2483b4a8369f4a8efd1ca35d9357bdcc1c43cdeb
Maxim Vinogradov <maxvinza@yandex.ru>
2020-03-11 15:11:56 +0100

    add bitwrap

--- src/es/pes/peshead.rs
+++ src/es/pes/peshead.rs
@@ -1,10 +1,20 @@
-#[derive(Default)]
+use bitwrap::BitWrap;
+
+
+#[derive(BitWrap, Default, Debug)]
 pub struct PESHead {
+    #[bits(1)]
     flag_error: bool,
+    #[bits(1)]
     flag_start: bool,
+    #[bits(1)]
     flag_priority: bool,
+    #[bits(13)]
     pid: u16,
+    #[bits(2)]
     scrembled: u8,
+    #[bits(2)]
     adaptation_field_control: u8,
+    #[bits(4)]
     cc: u8,
 }
