c6fda1bc2be43d157e88f86849fc83903442d833
Andrei Dyldin <and@cesbo.com>
2026-01-14 22:42:31 +0200

    eit: Bytes to to_be_bytes()

--- src/psi/eit.rs
+++ src/psi/eit.rs
@@ -1,5 +1,4 @@
 use crate::{
-    bytes::Bytes,
     psi::{
         Descriptors,
         Psi,
@@ -143,11 +142,14 @@ impl Eit {
     }
 
     fn psi_init(&self) -> Psi {
-        let mut psi = Psi::new(self.table_id, 14, self.version);
+        let mut psi = Psi::new(self.table_id, 3, self.version);
         psi.buffer[1] = 0xF0; // set reserved_future_use bit
-        psi.buffer[3 ..].set_u16(self.pnr);
-        psi.buffer[8 ..].set_u16(self.tsid);
-        psi.buffer[10 ..].set_u16(self.onid);
+        psi.buffer.extend_from_slice(&self.pnr.to_be_bytes()); // [3, 4]
+        psi.buffer.push(0xC0 | ((self.version << 1) & 0x3E) | 0x01); // [5]
+        psi.buffer.extend_from_slice(&[0x00, 0x00]); // [6, 7] placeholder
+        psi.buffer.extend_from_slice(&self.tsid.to_be_bytes()); // [8, 9]
+        psi.buffer.extend_from_slice(&self.onid.to_be_bytes()); // [10, 11]
+        psi.buffer.extend_from_slice(&[0x00, 0x00]); // [12, 13] placeholder
         psi
     }
 }
